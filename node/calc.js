ee94d31edf16b66ec796c060bb9f6945daf64a69
k941122 <941122k@naver.com>
2024-01-24 19:09:08 +0900

    node 기초 강의

---
+++ node/calc.js
@@ -0,0 +1,12 @@
+const add = (a,b) => a + b;
+const sub = (a,b) => a - b;
+
+module.exports = {
+    modulename : "calc module",
+    add : add,
+    sub : sub,
+};
+
+/**
+ * module.exports 작성한 모듈 내보내기
+ */
---
+++ node/index.js
@@ -0,0 +1,12 @@
+const calc = require("./calc.js");
+
+console.log(calc.add(1,2));
+console.log(calc.add(4,5));
+console.log(calc.add(10,2));
+
+/**
+ * 파일 경로를 잘 성정해야 하고 현재 내가 있는 폴더가
+ * 어딘지를 파악한 후에 cd "원하는 폴더명" 으로 이동한 후 
+ * js 파일을 실행해 줘야 한다.
+ 
+ */
