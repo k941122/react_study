cb67c80a89aaf01281f5a5b5643ea83a8189b538
k941122 <941122k@naver.com>
2024-01-25 20:03:42 +0900

    리엑트 기본 설명 및 JSX문법

---
+++ reactexam1/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  let name = "차은우";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 React!{name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
